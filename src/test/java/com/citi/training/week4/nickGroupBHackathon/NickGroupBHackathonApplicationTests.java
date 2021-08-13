a4de2d4e2da189224143f9c0aadf413289b1409d
Jung Rae Jang <jungrae.jang@gmail.com>
2021-08-13 14:03:55 +0000

    created new project

---
+++ src/test/java/com/citi/training/week4/nickGroupBHackathon/NickGroupBHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.training.week4.nickGroupBHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickGroupBHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
