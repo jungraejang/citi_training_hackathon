03962c7387757306d682bdc19eda44955b7818bf
Administrator <sp4681@nyu.edu>
2021-08-25 13:24:23 +0000

    updated sql script, fixed local db login

--- sql/hackathon_week4.sql
+++ sql/hackathon_week4.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE conygre;
 CREATE TABLE `conygre`.`investors` (
                                        `id` INT NOT NULL AUTO_INCREMENT,
                                        `name` VARCHAR(45) NOT NULL,
@@ -5,7 +6,7 @@ CREATE TABLE `conygre`.`investors` (
                                        PRIMARY KEY (`id`),
                                        UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
 
-DROP TABLE conygre.transactions;
+-- DROP TABLE conygre.transactions;
 
 CREATE TABLE `conygre`.`transactions`(
                                          `id` INT NOT NULL AUTO_INCREMENT,
@@ -30,9 +31,7 @@ insert into transactions values(3, 1, "Cash", 120000.34, 0, TIMESTAMP("2017-07-2
 insert into transactions values(4, 1, "Cash", 16999.34, 0, TIMESTAMP("2017-07-23",  "13:10:11"), "Citibank Checkings");
 
 insert into transactions values(5, 2, "Stock", 132.34, 10, TIMESTAMP("2017-07-23",  "13:10:11"), "AMD");
-insert into transactions values(6, 2, "Stock", 566.78, 12, TIMESTAMP("2017-09-23",  "13:10:11"), "NVD");
 insert into transactions values(7, 2, "Stock", 378.78, 100, TIMESTAMP("2017-11-01",  "13:10:11"), "AMG");
-insert into transactions values(8, 2, "Stock", 7.98, 1000, TIMESTAMP("2018-01-24",  "13:10:11"), "GGL");
 
 insert into transactions values(9, 2, "Cash", 2300.89, 0, TIMESTAMP("2021-08-19","3:34:10"),"Citibank Savings");
 insert into transactions values(10, 2, "Cash", 34000.89, 0, TIMESTAMP("2021-08-19","3:34:10"),"Citibank Checkings");
@@ -47,4 +46,6 @@ insert into transactions values(13, 3, "Cash", 3800.89, 0, TIMESTAMP("2021-08-19
 
 
 -- select * from transactions;
+
+
 
